0d230ab73fe30e1ebaf3f2d38517b82c56993865
Natalman <natalnahm@gmail.com>
2020-03-16 19:41:06 -0500

    Changed the background color of the body.

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,10 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.red.shade300,
       appBar: AppBar(
         title: Text("Ask Me Anything"),
-        backgroundColor: Colors.blue.shade900,
+        backgroundColor: Colors.blueGrey,
       ),
     body: Ball(),
     );
